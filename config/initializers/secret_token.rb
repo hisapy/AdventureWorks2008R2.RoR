d03796a5333f0a93786a219316ea2ce1de7483b3
hisa <kazutron@gmail.com>
2012-05-18 12:55:32 -0400

    RoR initial setup
    Simplemente el proyecto RoR y un archivo Readme.md con la descripción
    general del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdventureWorks2008R2Ror::Application.config.secret_token = 'bcae27894777d2032b5f4b9038538a0c47bc3148f2fce96f789a896a695594e479e7687f8179c08d860abb004a7f79ff0f311e6e9d16aa2a9727957cbf5ed5db'
